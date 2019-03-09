7fc47e8f6eb32aed95c069d84184a553f8381ed2
Crystal <crystalliang882@gmail.com>
2019-03-09 15:42:55 -0800

    js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  var canvas = document.getElementById('myCanvas');
+  var ctx = canvas.getContext('2d');
+
+  var stress = 0;
+  var maxStress = 100;
+
+  var progress = 0;
+  var maxProgress = 0;
+
+  var currentX = 10;
+  var currentY = 10;
+
+});
