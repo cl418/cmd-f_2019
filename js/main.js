e7e6a822bf269105c86f1bf996a159ce6d9991f9
Unknown <ania.leonova@gmail.com>
2019-03-09 19:33:32 -0700

    behold! the character moves!

--- js/main.js
+++ js/main.js
@@ -12,7 +12,49 @@
 
   var player = new Image();
   player.onload = function () {
-    ctx.drawImage(player, 200, 200);
+    init();
+    //ctx.drawImage(player, 200, 200);
   };
-  player.src = "./assets/alienPink_badge1.png";
+  player.src = "./assets/char.png";
   ctx.drawImage(player, 200, 200);
+
+  //player animation variables
+  const width = 33;
+  const height = 32;
+  const cycleLoop = [0, 1, 2, 1];
+  var currentLoopIndex = 0;
+  var frameCount = 0;
+
+  //draw one frame of the character
+  function drawFrame(frameX, frameY, canvasX, canvasY) {
+    ctx.drawImage(player,
+    frameX * width, frameY * height, 
+    width, height,
+    canvasX, canvasY,
+    width, height);
+  }
+
+  //one step of animation
+  function step() {
+    //to slow down the character
+    frameCount++;
+    if(frameCount < 15) { 
+      window.requestAnimationFrame(step);
+      return;
+    }
+    frameCount = 0;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    //draw the character frame
+    drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0);
+    currentLoopIndex++;
+    if (currentLoopIndex >= cycleLoop.length) {
+      currentLoopIndex = 0;
+    }
+    window.requestAnimationFrame(step);
+  }
+
+  //actual animation
+  function init() {
+    window.requestAnimationFrame(step);
+  }
